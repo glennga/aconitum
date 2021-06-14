65ae9942089ac06b433b5427858c35ff3663f779
ggalvizo <gjgalvizo@gmail.com>
2021-06-14 02:10:24 -0700

    Couchbase running with sample data.

--- README.md
+++ README.md
@@ -7,8 +7,27 @@ All experiments have been executed on AWS EC2 instances. Each node was of type `
 ### TPC CH(2)
 A modified TPC-CH was utilized here, one that a) more naturally represents orderlines within orders as nested documents, and b) has orderline dates that are uniformly distributed across 7 years. The parameters used for TPC-CH generator were `scaleFactor=1` and `numWarehouses=200`.
 
+### All Systems
+
+1. Install `python 3.8`, and `python3-pip`.
+2. Clone this repository onto the server node. Ensure you have the correct requirements.
+
+```bash
+# Clone the repository.
+git clone https://github.com/glennga/aconitum.git
+
+# Install the requirements (a virtual environment also works).
+cd aconitum
+python3 -m pip install -r requirements.txt
+```
+3. Ensure that your `python3` searches the `aconitum` repository for modules. Add the following line to your `.bashrc` file.
+
+```bash
+export PYTHONPATH=$PYTHONPATH:/home/ubuntu/aconitum
+```
+
 ### AsterixDB
-1. Ensure that AsterixDB is installed and configured on the node to run the experiments on. Install `java 11` , `python 3.8`, and `python3-pip`. The cc.conf file used is as follows:
+1. Ensure that AsterixDB is installed with `java 11` and configured on the node to run the experiments on. The cc.conf file used is as follows:
 ```
 [nc/asterix_nc1]
 txn.log.dir=txnlog
@@ -28,96 +47,146 @@ log.dir=logs/
 log.level=INFO
 ```
 
-2. Create the dataverse (`TPC_CH`) required for this experiment.
+2. Create the dataverse (`TPC_CH`) required for this experiment. We build two secondary indexes: one on the orderline delivery date and another on the stock item ID. 
 ```sql
-DROP DATAVERSE TPC_CH IF EXISTS;
-CREATE DATAVERSE TPC_CH;
-USE TPC_CH;
+DROP    DATAVERSE TPC_CH IF EXISTS;
+CREATE  DATAVERSE TPC_CH;
+USE     TPC_CH;
 
-CREATE TYPE CustomerType AS { c_w_id: bigint, c_d_id: bigint, c_id: bigint };
-CREATE DATASET Customer (CustomerType) PRIMARY KEY c_w_id, c_d_id, c_id;
+CREATE  TYPE CustomerType AS { c_w_id: bigint, c_d_id: bigint, c_id: bigint };
+CREATE  DATASET Customer (CustomerType) PRIMARY KEY c_w_id, c_d_id, c_id;
 
-CREATE TYPE NationType AS { n_nationkey: bigint };
-CREATE DATASET Nation (NationType) PRIMARY KEY n_nationkey;
+CREATE  TYPE NationType AS { n_nationkey: bigint };
+CREATE  DATASET Nation (NationType) PRIMARY KEY n_nationkey;
 
-CREATE TYPE OrdersType AS { o_w_id: bigint, o_d_id: bigint, o_id: bigint  };
-CREATE DATASET Orders (OrdersType) PRIMARY KEY o_w_id, o_d_id, o_id;
-
-CREATE TYPE StockType AS { s_w_id: bigint, s_i_id: bigint };
-CREATE DATASET Stock (StockType) PRIMARY KEY s_w_id, s_i_id;
-
-CREATE TYPE ItemType AS { i_id: bigint };
-CREATE DATASET Item (ItemType) PRIMARY KEY i_id;
-
-CREATE TYPE RegionType AS { r_regionkey: bigint };
-CREATE DATASET Region (RegionType) PRIMARY KEY r_regionkey;
-
-CREATE TYPE SupplierType AS { su_suppkey: bigint };
-CREATE DATASET Supplier (SupplierType) PRIMARY KEY su_suppkey; 
-
-CREATE INDEX orderlineDelivDateIdx ON Orders ( 
-  UNNEST o_orderline SELECT ol_delivery_d : string 
+CREATE  TYPE OrdersType AS { o_w_id: bigint, o_d_id: bigint, o_id: bigint  };
+CREATE  DATASET Orders (OrdersType) PRIMARY KEY o_w_id, o_d_id, o_id;
+CREATE  INDEX orderlineDelivDateIdx 
+ON      Orders ( 
+  UNNEST  o_orderline 
+  SELECT  ol_delivery_d : string
 );
-CREATE INDEX orderlineItemIdx ON Orders ( 
-  UNNEST o_orderline SELECT ol_i_id : bigint 
-);
+
+CREATE  TYPE StockType AS { s_w_id: bigint, s_i_id: bigint };
+CREATE  DATASET Stock (StockType) PRIMARY KEY s_w_id, s_i_id;
+CREATE  INDEX stockItemIdx
+ON      Stock ( s_i_id );
+
+CREATE  TYPE ItemType AS { i_id: bigint };
+CREATE  DATASET Item (ItemType) PRIMARY KEY i_id;
+
+CREATE  TYPE RegionType AS { r_regionkey: bigint };
+CREATE  DATASET Region (RegionType) PRIMARY KEY r_regionkey;
+
+CREATE  TYPE SupplierType AS { su_suppkey: bigint };
+CREATE  DATASET Supplier (SupplierType) PRIMARY KEY su_suppkey; 
 ```
 
 3. Load each dataset in the dataverse (`TPC_CH`). Adjust the path accordingly.
 
 ```sql
 LOAD DATASET TPC_CH.Customer USING localfs (
-  ("path"="localhost:///home/$USER/tpc_ch/customer.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/customer.json"), 
   ("format"="json") 
 );
 LOAD DATASET TPC_CH.Nation USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/nation.json"),
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/nation.json"),
   ("format"="json") 
 );
 LOAD DATASET TPC_CH.Orders USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/orders.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/orders.json"), 
   ("format"="json") 
 );
 LOAD DATASET TPC_CH.Stock USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/stock.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/stock.json"), 
   ("format"="json") 
 );    
 LOAD DATASET TPC_CH.Item USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/item.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/item.json"), 
   ("format"="json") 
 );            
 LOAD DATASET TPC_CH.Region USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/region.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/region.json"), 
   ("format"="json") 
 );                  
 LOAD DATASET TPC_CH.Supplier USING localfs ( 
-  ("path"="localhost:///home/$USER/tpc_ch/supplier.json"), 
+  ("path"="localhost:///home/ubuntu/aconitum/resources/sample/supplier.json"), 
   ("format"="json") 
 ); 
 ```
 
-4. Clone this repository onto the server node. Ensure you have the correct requirements.
-```bash
-# Clone the repository.
-git clone https://github.com/glennga/aconitum.git
+4. Execute the benchmark query suite for AsterixDB.
 
-# Install the requirements (a virtual environment also works).
-cd aconitum
-python3 -m pip install -r requirements.txt
+```bash
+python3 aconitum/_asterixdb.py
 ```
 
-5. Execute the benchmark query suite. Ensure that your `python3` searches the `aconitum` repository for modules.
+5. Analyze the results! The results will be stored in the `out` folder under `results.json` as single line JSON documents.
+
+### Couchbase
+
+1. Ensure that Couchbase 7.0 is installed and configured on the node to run the experiments on. Docs on the install can be found [here](https://docs.couchbase.com/server/7.0/install/ubuntu-debian-install.html). Use the deb package instructions to get the latest and greatest. The memory for the Data service should be maximized (~6050 MB), and the only other service that should be available is the Index service (512 MB).
+2. Create the bucket to hold all of your data. This should utilize the entire cluster's memory data quota (6050MB in this case). Change the bucket's ejection method policy from _Value-only_ to _Full_.
+3. Create the collections and indexes associated with each collection. The default scope of the bucket is used to house each collection.
+
+```sql
+CREATE  COLLECTION aconitum._default.Customer;
+CREATE  INDEX customerPrimaryKeyIdx 
+ON      aconitum._default.Customer ( c_w_id, c_d_id, c_id );
+
+CREATE  COLLECTION aconitum._default.Nation;
+CREATE  INDEX nationPrimaryKeyIdx 
+ON      aconitum._default.Nation ( n_nationkey );
+
+CREATE  COLLECTION aconitum._default.Orders;
+CREATE  INDEX ordersPrimaryKeyIdx
+ON      aconitum._default.Orders ( o_w_id, o_d_id, o_id );
+CREATE  INDEX orderlineDelivDateIdx 
+ON      aconitum._default.Orders (
+  DISTINCT  ARRAY OL.ol_delivery_d
+  FOR       OL 
+  IN        o_orderline 
+  END
+);
+            
+CREATE  COLLECTION aconitum._default.Stock;
+CREATE  INDEX stockPrimaryKeyIdx
+ON      aconitum._default.Stock ( s_w_id, s_i_id );
+  
+CREATE  COLLECTION aconitum._default.Item;
+CREATE  INDEX itemPrimaryKeyIdx
+ON      aconitum._default.Item ( i_id );
+CREATE  PRIMARY INDEX itemPrimaryIdx 
+ON      aconitum._default.Item; 
+
+CREATE  COLLECTION aconitum._default.Region;
+CREATE  INDEX regionPrimaryKeyIdx
+ON      aconitum._default.Region ( r_regionkey );
+
+CREATE  COLLECTION aconitum._default.Supplier;
+CREATE  INDEX supplierPrimaryKeyIdx
+ON      aconitum._default.Supplier ( su_suppkey );
+```
+
+4. Load each collection in the bucket. Adjust the path accordingly.
 
 ```bash
-export PYTHONPATH=$PYTHONPATH:/home/ubuntu/aconitum
+for c in customer nation orders stock item region supplier; do
+  /opt/couchbase/bin/cbimport json \
+    --cluster localhost --username "admin" --password "password" \
+    --bucket "aconitum" --scope-collection-exp _default.${c^} \
+    --dataset file:///home/ubuntu/aconitum/resources/sample/$c.json \
+    --format lines --generate-key key::#UUID#
+done
+```
 
-cd aconitum
-python3 aconitum/asterixdb.py
+5. Execute the benchmark query suite for Couchbase.
+
+```bash
+python3 aconitum/_couchbase.py
 ```
 
 6. Analyze the results! The results will be stored in the `out` folder under `results.json` as single line JSON documents.
-
-### Couchbase
 
 
 ### MongoDB
---
+++ aconitum/_couchbase.py
@@ -0,0 +1,224 @@
+import argparse
+import json
+import datetime
+import timeit
+
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster, ClusterOptions
+
+from aconitum.query import AbstractBenchmarkQueryRunnable, AbstractBenchmarkQuerySuite
+from aconitum.executor import AbstractBenchmarkRunnable
+
+
+class CouchbaseBenchmarkQuerySuite(AbstractBenchmarkQuerySuite):
+    def __init__(self, cluster, bucket_name, logger, **kwargs):
+        super().__init__(logger=logger, **kwargs)
+        self.cluster = cluster
+        self.keyspace_prefix = f'{bucket_name}._default'
+        self.logger = logger
+
+    def execute_n1ql(self, statement, timeout=None):
+        query_parameters = {} if timeout is None else {'timeout': datetime.timedelta(seconds=timeout)}
+        lean_statement = ' '.join(statement.split())
+        try:
+            response_iterable = self.cluster.query(lean_statement, **query_parameters)
+            response_json = {'statement': lean_statement, 'results': []}
+            response_json = {**response_json, **response_iterable.meta}
+            for record in response_iterable:
+                response_json['results'].append(record)
+
+        except Exception as e:
+            self.logger.warning(f'Status of executing statement {statement} not successful, but instead {e}.')
+            response_json = {'statement': lean_statement, 'results': [], 'error': str(e), 'status': 'timeout'}
+
+        return response_json
+
+    def query_0_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query0Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query0Runnable, self).__init__('0')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    FROM        {self.query_suite.keyspace_prefix}.Orders O
+                    UNNEST      O.o_orderline OL
+                    WHERE       OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}"
+                    SELECT      COUNT(*) AS count_order;
+                """, timeout=timeout)
+
+        return _Query0Runnable(query_suite=self)
+
+    def query_1_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query1Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query1Runnable, self).__init__('1')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    FROM        {self.query_suite.keyspace_prefix}.Orders O
+                    UNNEST      O.o_orderline OL
+                    WHERE       OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}"
+                    GROUP BY    OL.ol_number
+                    SELECT      OL.ol_number, SUM(OL.ol_quantity) AS sum_qty, SUM(OL.ol_amount) AS sum_amount,
+                                AVG(OL.ol_quantity) AS avg_qty, AVG(OL.ol_amount) AS avg_amount, 
+                                COUNT(*) AS count_order
+                    ORDER BY    OL.ol_number;
+                """, timeout=timeout)
+
+        return _Query1Runnable(query_suite=self)
+
+    def query_6_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query6Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query6Runnable, self).__init__('6')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    FROM    {self.query_suite.keyspace_prefix}.Orders O
+                    UNNEST  O.o_orderline OL
+                    WHERE   OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}" AND 
+                            OL.ol_quantity BETWEEN 1 AND 100000
+                    SELECT  SUM(OL.ol_amount) AS revenue;
+                """, timeout=timeout)
+
+        return _Query6Runnable(query_suite=self)
+
+    def query_7_factory(self) -> AbstractBenchmarkQueryRunnable:
+        return self.NoOpBenchmarkQueryRunnable('7')
+
+    def query_12_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query12Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query12Runnable, self).__init__('12')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    FROM        {self.query_suite.keyspace_prefix}.Orders O
+                    UNNEST      O.o_orderline OL
+                    WHERE       O.o_entry_d <= OL.ol_delivery_d AND 
+                                OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}"
+                    GROUP BY    O.o_ol_cnt
+                    SELECT      O.o_ol_cnt, 
+                                SUM(CASE WHEN O.o_carrier_id = 1 OR O.o_carrier_id = 2 
+                                         THEN 1 ELSE 0 END) AS high_line_count,
+                                SUM(CASE WHEN O.o_carrier_id <> 1 OR O.o_carrier_id <> 2 
+                                         THEN 1 ELSE 0 END) AS low_line_count
+                    ORDER BY    O.o_ol_cnt;
+                """, timeout=timeout)
+
+        return _Query12Runnable(query_suite=self)
+
+    def query_14_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query14Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query14Runnable, self).__init__('14')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    FROM    {self.query_suite.keyspace_prefix}.Orders O
+                    UNNEST  O.o_orderline OL
+                    JOIN    {self.query_suite.keyspace_prefix}.Item I
+                    ON      I.i_id = OL.ol_i_id
+                    WHERE   OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}"
+                    SELECT  100.00 * SUM(CASE WHEN I.i_data LIKE 'pr%' THEN OL.ol_amount ELSE 0 END) / 
+                                (1 + SUM(OL.ol_amount)) AS promo_revenue;
+                """, timeout=timeout)
+
+        return _Query14Runnable(query_suite=self)
+
+    def query_15_factory(self) -> AbstractBenchmarkQueryRunnable:
+        class _Query15Runnable(AbstractBenchmarkQueryRunnable):
+            def __init__(self, query_suite):
+                super(_Query15Runnable, self).__init__('15')
+                self.query_suite = query_suite
+
+            def invoke(self, date_1, date_2, timeout) -> dict:
+                return self.query_suite.execute_n1ql(f"""
+                    WITH        Revenue AS (
+                                FROM        {self.query_suite.keyspace_prefix}.Orders O
+                                UNNEST      O.o_orderline OL
+                                JOIN        {self.query_suite.keyspace_prefix}.Stock S
+                                ON          OL.ol_i_id = S.s_i_id AND OL.ol_supply_w_id = S.s_w_id
+                                WHERE       OL.ol_delivery_d BETWEEN "{date_1}" AND "{date_2}"
+                                GROUP BY    ((S.s_w_id * S.s_i_id) % 10000)
+                                SELECT      ((S.s_w_id * S.s_i_id) % 10000) AS supplier_no, 
+                                            SUM(OL.ol_amount) AS total_revenue
+                    )
+                    FROM        Revenue R
+                    JOIN        {self.query_suite.keyspace_prefix}.Supplier SU
+                    ON          SU.su_suppkey = R.supplier_no
+                    WHERE       R.total_revenue = ( 
+                                FROM        Revenue M
+                                SELECT      VALUE MAX (M.total_revenue)
+                    )[0]
+                    SELECT      SU.su_suppkey, SU.su_name, SU.su_address, SU.su_phone, R.total_revenue
+                    ORDER BY    SU.su_suppkey;
+                """, timeout=timeout)
+
+        return _Query15Runnable(query_suite=self)
+
+    def query_20_factory(self) -> AbstractBenchmarkQueryRunnable:
+        return self.NoOpBenchmarkQueryRunnable('20')
+
+
+class CouchbaseBenchmarkRunnable(AbstractBenchmarkRunnable):
+    @staticmethod
+    def _collect_config(**kwargs):
+        parser = argparse.ArgumentParser(description='Benchmark TPC_CH queries on an Couchbase instance.')
+        parser.add_argument('--config', type=str, default='config/couchbase.json', help='Path to the config file.')
+        parser.add_argument('--datagen', type=str, default='config/tpc_ch.json', help='Path to the datagen file.')
+        parser.add_argument('--aconitum', type=str, default='config/aconitum.json', help='Path to the experiment file.')
+        parser.add_argument('--notes', type=str, default='', help='Any notes to append to each log entry.')
+        parser_args = parser.parse_args()
+
+        # Load all configuration files into a single dictionary.
+        with open(parser_args.config) as config_file:
+            config_json = json.load(config_file)
+        with open(parser_args.datagen) as datagen_file:
+            config_json['tpcCH'] = json.load(datagen_file)
+        with open(parser_args.aconitum) as experiment_file:
+            config_json['experiment'] = json.load(experiment_file)
+
+        # Specify the results directory.
+        config_json['resultsDir'] = 'out/' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '-' + \
+                                    'CouchbaseTPCCHQuery'
+
+        # Return all relevant information to our caller.
+        return {'workingSystem': 'Couchbase', 'runtimeNotes': parser_args.notes, **config_json, **kwargs}
+
+    def __init__(self, **kwargs):
+        super().__init__(**self._collect_config(**kwargs))
+
+        # Connect to our Couchbase cluster.
+        self.cluster_uri = 'couchbase://' + self.config['cluster']['address']
+        self.bucket_name = self.config['cluster']['bucket']
+        self.cluster = Cluster.connect(self.cluster_uri, ClusterOptions(PasswordAuthenticator(
+            username=self.config['username'],
+            password=self.config['password']
+        )))
+
+    def perform_benchmark(self):
+        for i in range(self.config['experiment']['repeat']):
+            for sigma in self.config['experiment']['sigmaValues']:
+                for query in CouchbaseBenchmarkQuerySuite(
+                    cluster=self.cluster,
+                    bucket_name=self.bucket_name,
+                    logger=self.logger,
+                    **self.config['tpcCH']
+                ):
+                    # Execute the query. Record the client response time.
+                    self.logger.info(f'Executing query {query} with sigma {sigma} @ run {i + 1}.')
+                    t_before = timeit.default_timer()
+                    results = query(sigma=sigma, timeout=self.config['experiment']['timeout'])
+                    results['clientTime'] = timeit.default_timer() - t_before
+                    results['runNumber'] = i
+                    self.log_results(results)
+
+
+if __name__ == '__main__':
+    CouchbaseBenchmarkRunnable().invoke()
